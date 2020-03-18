1f535a17327f459daae786071cfdbf4aa4b97baf
Wesley Flôres <ti06@alfasistemas.inf.br>
2020-03-18 13:23:33 -0300

    #5 Problem

---
+++ Project Euler.NET - Problem 005/Project Euler.NET - Problem 005.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+const int MAX = 20;
+
+int main(int argc, char *argv[])
+{
+    int arr[MAX] = {0}, i = 1, MULTIPLICADOR = 1, PRODUTO = 1;
+
+    unsigned long int MIN = 1;
+
+    for (int i = 0; i < MAX; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+    while (true)
+    {
+        MULTIPLICADOR = 1;
+
+        PRODUTO = 1;
+
+        for (int k = 0; k < MAX; k++)
+        {
+            if (arr[k] % i == 0)
+            {
+                arr[k] /= i;
+
+                MULTIPLICADOR = i;
+            }
+
+            PRODUTO *= arr[k];
+        }
+
+        MIN *= MULTIPLICADOR;
+
+        if (MULTIPLICADOR == 1)
+        {
+            i++;
+        }
+
+        if (PRODUTO == 1)
+        {
+            break;
+        }
+    }
+
+    std::cout << MIN << std::endl;
+}
