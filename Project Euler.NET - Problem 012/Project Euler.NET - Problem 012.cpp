91782deb84bf739ab2392606f504cb37d9894f35
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-07-31 23:37:09 -0300

    Problem #12

--- Project Euler.NET - Problem 012/Project Euler.NET - Problem 012.cpp
+++ Project Euler.NET - Problem 012/Project Euler.NET - Problem 012.cpp
@@ -1,79 +1,51 @@
 #include <iostream>
-#include <cmath>
 
-const int DIMENSION = 3;
+const int LIMIT = 500;
 
-int POSITION(int n, int j, int k, int m)
+unsigned long long int triangle_number(int N)
 {
-    int INIT = n;
+    return (N * (N - 1)) / 2;
+}
 
-    for (int i = m - 1; i >= 0; i--)
+int Div(int n)
+{
+    int NUMBER = 0;
+
+    for (int i = 1; i < n; i++)
     {
-        const int DIVISOR = INIT / pow(m, i);
-
-        INIT = INIT - DIVISOR * pow(m, i);
-
-        if (j == i)
+        if (n % i == 0)
         {
-            return DIVISOR;
+            NUMBER++;
         }
     }
 
-    return 0;
+    return NUMBER;
 }
 
 int main(int argc, char *argv[])
 {
-    int A[DIMENSION] = {0}, COMBINATION = pow(DIMENSION, DIMENSION), COUNTER = 0;
+    int __MAX = 0;
 
-    int **SWAP = new int *[COMBINATION];
-
-    for (int i = 0; i < COMBINATION; i++)
+    for (int i = 0; true; i++)
     {
-        SWAP[i] = new int[COMBINATION];
-    }
+        int __TRIANGLE_NUMBER = triangle_number(i);
 
-    for (int i = 2; COUNTER < DIMENSION; i++)
-    {
-        int ASSERT = 0;
+        int __DIVISORS = Div(__TRIANGLE_NUMBER);
 
-        for (int j = 1; j <= i; j++)
+        if (__DIVISORS > __MAX)
         {
-            if (i % j == 0)
-            {
-                ASSERT++;
-            }
+            __MAX = __DIVISORS;
+
+            std::cout << i << " " << __TRIANGLE_NUMBER << " " << __DIVISORS << std::endl;
         }
 
-        if (ASSERT <= 2)
+        if (__DIVISORS > LIMIT)
         {
-            A[COUNTER] = i;
-
-            COUNTER++;
-        }
-
-        ASSERT = 0;
-    }
-
-    for (int i = 0; i < pow(DIMENSION, DIMENSION); i++)
-    {
-        for (int j = 0; j < DIMENSION; j++)
-        {
-            const int INDEX = POSITION(i, j, COMBINATION, DIMENSION);
-
-            SWAP[i][j] = A[INDEX];
+            break;
         }
     }
 
-    for (int i = 0; i < pow(DIMENSION, DIMENSION); i++)
-    {
-        for (int j = 0; j < DIMENSION; j++)
-        {
-            std::cout << SWAP[i][j] << " ";
-        }
-
-        std::cout << std::endl;
-    }
+    std::cout << __MAX << std::endl;
 
     return 0;
 }
